7f3ed80f6b680f9e14f89358929f488d97cd2cfc
baodeptrai912 <dangson2376@gmail.com>
2026-06-14 17:20:15 +0700

    Deployment testing

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,15 @@ import routes from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import { connectDb } from "../config/db.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
-app.get("/", (req, res) => {
-  res.send("hello sơn");
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 await connectDb();
 app.use(cookieParser());
 app.use(express.json());
@@ -20,6 +23,12 @@ app.use(
   }),
 );
 routes(app);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`The server is running at http://localhost:${PORT} `);
