3afae7d1c973d98b043f403ea1e817fa51aa7cbe
baodeptrai912 <dangson2376@gmail.com>
2026-06-14 17:32:57 +0700

    Add development feature

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 const client = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL:
+    import.meta.env.NODE_ENVIRONMENT === "development"
+      ? import.meta.env.VITE_BASE_URL
+      : "/api",
   withCredentials: true,
 });
 export default client;
